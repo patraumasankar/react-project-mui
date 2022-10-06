b78643a2dac8e515cbe9d486eacf21c841f2007e
Umasankar <patraumasankar18@gmail.com>
2022-10-06 21:35:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Navbar, Footer, Home, About, Contact, Service, Blog } from './component';
+import { Navbar, Footer, Home, Login, Signup, About, Contact, Service, Blog } from './component';
 
 const App = () => {
   return (
@@ -10,6 +10,8 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/home' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/service' element={<Service />} />
--- src/component/index.js
+++ src/component/index.js
@@ -1,6 +1,8 @@
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
 export { default as Home } from './Home';
+export { default as Login } from './Login';
+export { default as Signup } from './Signup';
 export { default as About } from './About';
 export { default as Contact } from './Contact';
 export { default as Service } from './Service';
