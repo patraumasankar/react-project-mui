9241f7a6c0be5668989bf65b7c0781fbe703009a
Umasankar <patraumasankar18@gmail.com>
2022-10-19 19:09:59 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Navbar, Footer, Home, Login, Signup, About, Contact, Service, Blog } from './component';
-import './App.css'
-
 const App = () => {
   return (
     <div>
